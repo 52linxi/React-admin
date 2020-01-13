3276ade6f600cbb94c602ed6ecd916bc26a9c4dd
fucheng <1031965725@qq.com>
2020-01-13 14:21:23 +0800

    完成登录发送请求

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import { Form, Icon, Input, Button } from "antd";
+import { Form, Icon, Input, Button, message } from "antd";
+import axios from "axios";
 
 import logo from "./logo.png";
 import "./index.less";
+const { Item } = Form;
+
 @Form.create()
 class login extends Component {
   //自定义表单正则
@@ -28,6 +31,44 @@ class login extends Component {
     //必须要调用一次
     callback();
   };
+  //提交数据逻辑
+  Login = e => {
+    //禁止默认行为
+    e.preventDefault();
+    //validateFields方法  检验表单正则和收集表单数据
+    this.props.form.validateFields((err, values) => {
+      console.log(err, values);
+
+      if (!err) {
+        //请求参数
+        const { username, password } = values;
+        // 发送请求
+        axios
+          .post("/api/login", { username, password })
+          .then(response => {
+            // 判断是否登录成功
+            if (response.data.status === 0) {
+              //跳转home 使用replace 不用保留登录历史记录
+              this.props.history.replace("/");
+            } else {
+              // 提示登录错误
+              message.error(response.data.msg);
+
+              // 清空密码
+              this.props.form.resetFields(["password"]);
+            }
+          })
+          .catch(err => {
+            // 打印错误给开发人员看
+            console.log(err);
+            // 提示错误给用户看
+            message.error("网络错误~");
+            // 清空密码
+            this.props.form.resetFields(["password"]);
+          });
+      }
+    });
+  };
 
   render() {
     //getFieldDecorator 方法 表单正则
@@ -40,8 +81,8 @@ class login extends Component {
         </header>
         <section className="login-section">
           <h3>用户登录</h3>
-          <Form className="login-form">
-            <Form.Item>
+          <Form className="login-form" onSubmit={this.Login}>
+            <Item>
               {getFieldDecorator("username", {
                 rules: [
                   {
@@ -51,13 +92,13 @@ class login extends Component {
               })(
                 <Input
                   prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
                   }
                   placeholder="用户名"
                 />
               )}
-            </Form.Item>
-            <Form.Item>
+            </Item>
+            <Item>
               {getFieldDecorator("password", {
                 rules: [
                   {
@@ -72,12 +113,16 @@ class login extends Component {
                   placeholder="密码"
                 />
               )}
-            </Form.Item>
-            <Form.Item>
-              <Button className="login-form-btn" type="primary">
+            </Item>
+            <Item>
+              <Button
+                className="login-form-btn"
+                type="primary"
+                htmlType="submit"
+              >
                 登录
               </Button>
-            </Form.Item>
+            </Item>
           </Form>
         </section>
       </div>
